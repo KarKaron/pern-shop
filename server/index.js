ef5209cc8269b5285f0fc77529416ad50e8bfb32
KarKaron <karkotski@gmail.com>
2021-04-20 18:44:47 +0300

    create and add database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,28 @@
 require("dotenv").config();
 
 const express = require("express"),
-      PORT = process.env.PORT || 5000,
-      app = express();
+      cors = require("cors"),
+      sequelize = require("./db"),
+      models = require("./models/models");
 
-app.listen(PORT, () => console.log(`SERVER START ON PORT ${PORT}`));
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ "message": "WORKING!!!"});
+});
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`SERVER START ON PORT ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
